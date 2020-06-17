a34a49b55db9ba913f1f8015df69de437bd7260d
Mark Lucking <mark.lucking@gmail.com>
2020-06-17 21:51:43 +0200

    Initial Commit

---
+++ LayoutDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutDemo
+//
+//  Created by localadmin on 17.06.20.
+//  Copyright © 2020 Mark Lucking. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
